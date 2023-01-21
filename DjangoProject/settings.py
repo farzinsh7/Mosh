3577e540cc15e351c65d717860be50ac488d63d6
Farzin Shams <f.shams@ehsan-group.com>
2023-01-21 17:55:34 +0330

    Create Like app

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'store',
     'tags',
+    'like',
 ]
 
 MIDDLEWARE = [
---
+++ like/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete = models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
