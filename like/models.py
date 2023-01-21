3fb77a071322ffe989a068dfc9b2ebda8884856f
Farzin Shams <f.shams@ehsan-group.com>
2023-01-21 18:56:01 +0330

    Migration

--- like/models.py
+++ like/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
--- store/models.py
+++ store/models.py
@@ -13,8 +13,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     las_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
@@ -39,6 +40,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
+
 
 
 class Order(models.Model):
@@ -67,6 +69,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255, null = True)
     customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key =True) #one to one Rellation
 
 
