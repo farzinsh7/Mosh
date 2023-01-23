ada6f79cda190f8524a0fae453aac9b1faa48d0e
farzinsh7 <farzinnater@gmail.com>
2023-01-23 23:29:51 +0330

    Install MySQL

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'NAME': 'ROOT',
+        'PASSWORD': 'Farzin1369007'
     }
 }
 
