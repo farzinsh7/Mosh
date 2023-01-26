d9714a3e3ddd1e25cf5711580df499899e53a723
Farzin Shams <f.shams@ehsan-group.com>
2023-01-26 11:59:21 +0330

    filter by range

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name':'Farzin'})
+    queryset = Product.objects.filter(unit_price__range=(20,30))
+    
+
+    return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
