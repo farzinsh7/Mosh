c7e2dcb5494f12d170a76d39ae8b3d84131ed432
farzinsh7 <farzinnater@gmail.com>
2023-01-27 15:38:15 +0330

    Selecting_Field_to_query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    queryset = Product.objects.all()[:5]
-    
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
 
     return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
