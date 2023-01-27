02c8860869634c03d298caeb22813781dc51104d
farzinsh7 <farzinnater@gmail.com>
2023-01-27 15:17:14 +0330

    Limiting_Results

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q, F
 from store.models import Product
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory=F('unit_price'))
+    queryset = Product.objects.all()[:5]
     
 
     return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
