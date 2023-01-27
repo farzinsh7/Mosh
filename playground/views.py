7d2252a54152b8c6d5a0082a032bf61e5b2b5f0c
farzinsh7 <farzinnater@gmail.com>
2023-01-27 14:59:42 +0330

    End Filtering

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, Customer
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory__lt=10)
+    queryset = OrderItem.objects.filter(product__collection__id=3)
     
 
     return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
