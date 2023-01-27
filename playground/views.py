fbddd8332329376a16a823170757d6a966a5bb6f
farzinsh7 <farzinnater@gmail.com>
2023-01-27 20:21:14 +0330

    Select related

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product,  Order
 
 def say_hello(request):
-    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
-    return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name':'Farzin', 'orders': list(queryset)})
