7735b17476f4284938bdc7a0a65fec17483a7114
farzinsh7 <farzinnater@gmail.com>
2023-01-27 20:46:02 +0330

    aggregating exersice

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Min, Max
-from store.models import Product,  Order
+from django.db.models.aggregates import Count, Min, Max, Sum, Avg
+from store.models import Product,  Order, OrderItem
 
 def say_hello(request):
-    result =Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'), max_price=Max('unit_price'))
+    result = Product.objects.filter(collection_id=3).aggregate(min_price=Min('unit_price'), max_price=Max('unit_price'), avg_price=Avg('unit_price'))
     return render(request, 'hello.html', {'name':'Farzin', 'result': result})
