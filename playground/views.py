674d0c19d249bc13932b30928b5bc8f6a42d2710
farzinsh7 <farzinnater@gmail.com>
2023-01-27 15:10:59 +0330

    Refrencing_Fields_Using_F_objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 def say_hello(request):
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
     
 
     return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
