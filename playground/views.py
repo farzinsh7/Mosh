9f1c029d629ce52539650b552bac00241d68dd7b
farzinsh7 <farzinnater@gmail.com>
2023-01-27 15:07:27 +0330

    Complex_lookups_using_Q_objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from django.db.models import Q
+from store.models import Product
 
 def say_hello(request):
-    queryset = OrderItem.objects.filter(product__collection__id=3)
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     
 
     return render(request, 'hello.html', {'name':'Farzin', 'products': list(queryset)})
