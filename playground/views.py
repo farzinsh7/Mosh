a62e8d9bbd570918b43db243af4870fc0d69e227
Farzin Shams <f.shams@ehsan-group.com>
2023-01-28 14:52:23 +0330

    Grouping_data

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
-from django.db.models.functions import Concat
+from django.db.models import Value, F, Func, Count
 from store.models import Product,  Order, OrderItem, Customer
 
 def say_hello(request):
-    queryset_1 = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name') , function='CONCAT'))
-    queryset = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
+    queryset = Customer.objects.annotate(orders_count=Count('order'))
     return render(request, 'hello.html', {'name':'Farzin', 'queryset': list(queryset)})
