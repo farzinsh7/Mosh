24ee19a1a74ef10c5cb1c67282c7edcfb79d58f6
Farzin Shams <f.shams@ehsan-group.com>
2023-01-28 16:00:30 +0330

    Expression_Wrapper

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count
-from store.models import Product,  Order, OrderItem, Customer
+from django.db.models import Value, F, ExpressionWrapper, DecimalField
+from store.models import Product,  Order
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(orders_count=Count('order'))
+    discounted_price = ExpressionWrapper(
+        F('unit_price') * 0.8, output_field=DecimalField()
+        ) 
+
+    queryset = Product.objects.annotate(
+        discounted_price=discounted_price
+        )
+
     return render(request, 'hello.html', {'name':'Farzin', 'queryset': list(queryset)})
