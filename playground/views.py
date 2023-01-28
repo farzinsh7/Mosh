455c67540f0b2a6b6c4fbb91ca1ac9c4a8bb280e
Farzin Shams <f.shams@ehsan-group.com>
2023-01-28 16:52:14 +0330

    Custom_Manager

--- playground/views.py
+++ playground/views.py
@@ -4,9 +4,6 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request): 
-    content_type = ContentType.objects.get_for_model(Product)
-    queryset = TaggedItem.objects.select_related('tag').filter(
-        content_type=content_type,
-        object_id=1
-    )
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
+    
     return render(request, 'hello.html', {'name':'Farzin', 'tags': list(queryset)})
