567ccc44f9c951f7c1052a327681ed46cd5655d9
Farzin Shams <f.shams@ehsan-group.com>
2023-01-28 16:22:31 +0330

    ORM annotating

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-from store.models import Product,  Order
+from django.db.models import Value, F, ExpressionWrapper, DecimalField, Max, Count, Sum
+from store.models import Product,  Order, Customer, Collection
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(
-        F('unit_price') * 0.8, output_field=DecimalField()
-        ) 
-
-    queryset = Product.objects.annotate(
-        discounted_price=discounted_price
-        )
+    queryset = Product.objects.annotate(total_sell = Sum(F('orderitem__unit_price') * F('orderitem__quantity'))).order_by('-total_sell')[:5]
 
     return render(request, 'hello.html', {'name':'Farzin', 'queryset': list(queryset)})
