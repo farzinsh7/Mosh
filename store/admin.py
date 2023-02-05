35301d5ce2c5a18420add5c76e4f9745e13f5927
farzinsh7 <farzinnater@gmail.com>
2023-02-05 22:45:01 +0330

    Using_generic_relations

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from . import models
+from tags.models import TaggedItem
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -17,10 +19,17 @@ class InventoryFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value() == '<10':
             return queryset.filter(inventory__lt = 10)
-    
+
+
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+    extra = 0
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
     autocomplete_fields = ['collection']
     prepopulated_fields = {
         'slug': ['title']
--- tags/models.py
+++ tags/models.py
@@ -17,6 +17,10 @@ class TagItemManager(models.Manager):
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.label
+    
+
 
 class TaggedItem(models.Model):
     objects = TagItemManager()
