04c1423430f569a1aae9fcb1f238b77d3465561d
farzinsh7 <farzinnater@gmail.com>
2023-02-05 22:12:31 +0330

    Adding_data_validator

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -19,12 +20,16 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2,
+        validators=[MinValueValidator(1)]
+        )
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
-    promotion = models.ManyToManyField(Promotion)
+    promotion = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self):
         return self.title
