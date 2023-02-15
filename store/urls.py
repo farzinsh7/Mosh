bc36aee76e5251c2d769328e4cc458d47aa11333
farzinsh7 <farzinnater@gmail.com>
2023-02-15 22:54:24 +0330

    Routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name = 'collection-detail'),
-]
+urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 
 
@@ -18,18 +18,16 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self): 
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id= kwargs['pk']).count() > 0:
             return Response({'error':'Product can not be deleted beacuse it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
     
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count = Count('products'))
     serializer_class = CollectionSerializer
-    
+
     def delete(self, request, pk):
         collection = get_object_or_404(Collection.objects.annotate(products_count = Count('products')), pk=pk)
         if collection.products.count() > 0:
