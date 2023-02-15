2fc0ed2665244692e51a496e3d839df3c8e49ec5
farzinsh7 <farzinnater@gmail.com>
2023-02-15 22:28:57 +0330

    View_Set

--- store/views.py
+++ store/views.py
@@ -6,23 +6,18 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     def get_serializer_context(self): 
         return {'request': self.request}
 
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
@@ -30,14 +25,9 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-class CollectionList(ListCreateAPIView):
+    
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count = Count('products'))
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset= Collection.objects.annotate(products_count = Count('products'))
     serializer_class = CollectionSerializer
     
     def delete(self, request, pk):
