86affa330888d5d419b23f0f45c2bc5fa56119d2
farzinsh7 <farzinnater@gmail.com>
2023-02-24 20:16:59 +0330

    Creating_User_Profiles

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -49,19 +51,25 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD,'Gold'),
     ]
 
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-
+    @admin.display(ordering='user__first_name')
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
+    @admin.display(ordering='user__last_name')
+    def first_name(self):
+        return self.user.first_name
     
+    def last_name(self):
+        return self.user.last_name
+
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 
